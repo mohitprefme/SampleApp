20ce81528f9f3cad282a101ecb0021c751d496ae
Mohit Moonat <m.moonat@prefme.com>
2021-09-28 12:49:35 +0530

    First Commit

---
+++ SampleTestApp/Shared/SampleTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleTestAppApp.swift
+//  Shared
+//
+//  Created by Mohit Moonat on 28/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
